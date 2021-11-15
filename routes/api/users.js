f052c14d3b342eccd60d1378b041dae39be957b8
Matt Swedin <swedin.matthew@gmail.com>
2021-11-15 00:25:34 -0500

    adding Tweets

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,11 +4,20 @@ const User = require('../../models/User')
 const bcrypt = require("bcryptjs")
 const keys = require('../../config/keys')
 const jwt = require('jsonwebtoken')
+const passport = require("passport")
+const validateRegisterInput = require('../../validation/register')
+const validateLoginInput = require('../../validation/login')
 
 router.get("/test", (req, res) => 
     res.json({ msg: "This is the users route" }));
 
 router.post('/register', (req, res) => {
+    const { errors, isValid } = validateRegisterInput(req.body);
+
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
     User.findOne({ email: req.body.email })
     .then(user => {
         if (user) {
@@ -34,9 +43,15 @@ router.post('/register', (req, res) => {
 })
 
 router.post('/login', (req, res) => {
+    const { errors, isValid } = validateLoginInput(req.body)
+
+     if (!isValid) {
+            return res.status(400).json(errors);
+        }
+
     const email = req.body.email
     const password = req.body.password
-
+       
     User.findOne({ email })
         .then(user => {
             if(!user) {
